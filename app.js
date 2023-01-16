a6abef5571f719663c574f50688a3f1b685ee879
Sharp-coder <117237448+mustafa-tj@users.noreply.github.com>
2023-01-16 13:22:52 +0530

    Add files via upload
    completed project todolist

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"))
+
+let items = ["Buy Food","Cook Food","Eat Food"];
+["Buy Food","Cook Food","Eat Food"];
+
+app.get("/",function(req,res){
+    let today = new Date();
+    
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month:"long"
+    };
+
+    let day = today.toLocaleDateString("en-US",options);
+
+    
+    res.render("list",{kindOfDay:day , newListItems:items});
+});
+
+app.post("/",function(req,res){
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/")
+})
+
+
+
+app.listen(3000,function(req,res){
+    console.log("server started");
+});
